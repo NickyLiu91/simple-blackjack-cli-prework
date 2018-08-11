e2eae6133fae146f54987accaf5fb657f93950d6
Nicky Liu <nickyliu91@gmail.com>
2018-08-11 05:42:34 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -16,19 +16,31 @@ def prompt_user
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(number)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  card1 = deal_card
+  card2 = deal_card
+  total = card1 + card2
+  display_card_total(total)
+  total
 end
 
-def hit?
-  # code hit? here
+def hit?(total)
+  prompt_user
+  input = get_user_input
+  if input == 'h'
+    total = total += deal_card
+  elsif input != 'h' || input != 's'
+    puts "Please enter a valid command."
+    prompt_user
+  end
+  total
 end
 
 def invalid_command
